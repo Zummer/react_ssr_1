063c3f687e04386a41377889d39b4b936dd38288
Afanasev <afanvt@gmail.com>
2025-10-24 16:27:46 +0700

    добавил верстку

--- src/Components/MainPage/index.tsx
+++ src/Components/MainPage/index.tsx
@@ -1,18 +1,42 @@
-import style from './style.less';
-import mainImage from '@/assets/djiagrast40-flying-over-field-540.jpg';
-import Logo from '@/assets/logo.svg';
-import Name from '@/assets/name.svg';
+import style from "./style.less";
+import mainImage from "@/assets/djiagrast40-flying-over-field-540.jpg";
+import Logo from "@/assets/logo.svg";
+import Name from "@/assets/name.svg";
+import classnames from 'classnames';
 
 export const MainPage = () => {
     return (
-        <div className={style.headerImg} style={{backgroundImage: `url(${mainImage})`}}>
-            <div className={style.headerLogo}>
-                <Logo className={style.logo} />
-                <div className={style.headerText}>
-                    <Name width={500} height={100}/>
-                    <div className={style.slogan}>Мы воплощаем самые смелые мечты в реальность</div>
+        <>
+            <div
+                className={style.headerImg}
+                style={{ backgroundImage: `url(${mainImage})` }}
+            >
+                <div className={style.headerLogo}>
+                    <Logo className={style.logo} />
+                    <div className={style.headerText}>
+                        <Name width={500} height={100} />
+                        <div className={style.slogan}>
+                            Мы воплощаем самые смелые мечты в реальность
+                        </div>
+                    </div>
+                </div>
+                <div className={classnames(style.info, style.columnContainer)}>
+                    <div className={style.headerAgro}>Агродрон</div>
+                    <div className={style.aboutAgro}>
+                        ключевой инструмент для соблюдения графика внесений СЗР,
+                        десикации, питания, в критические фазы
+                    </div>
                 </div>
             </div>
-        </div>
-    )
-}
+            <div className={style.columnContainer}>
+                <p>{`Агродрон как важный инструмент для поддержания 
+                    сельскохозяйственного производства путем внесения средств защиты растений (СЗР),
+                    десикации и питания растений в ключевые периоды роста. Это устройство 
+                    позволяет эффективно соблюдать график обработок, 
+                    что особенно актуально в условиях меняющихся погодных условий и 
+                    необходимости точного контроля над растениями.`}
+                 </p>
+            </div>
+        </>
+    );
+};
