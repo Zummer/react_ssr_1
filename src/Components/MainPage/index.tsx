7142ef5944a01abb0910e6bb400285e9aba89ace
Afanasev <afanvt@gmail.com>
2025-10-24 21:44:25 +0700

    верстка шапки для мобилки

--- src/Components/MainPage/index.tsx
+++ src/Components/MainPage/index.tsx
@@ -2,7 +2,7 @@ import style from "./style.less";
 import mainImage from "@/assets/djiagrast40-flying-over-field-540.jpg";
 import Logo from "@/assets/logo.svg";
 import Name from "@/assets/name.svg";
-import classnames from 'classnames';
+import classnames from "classnames";
 
 export const MainPage = () => {
     return (
@@ -11,13 +11,18 @@ export const MainPage = () => {
                 className={style.headerImg}
                 style={{ backgroundImage: `url(${mainImage})` }}
             >
-                <div className={style.headerLogo}>
-                    <Logo className={style.logo} />
-                    <div className={style.headerText}>
-                        <Name width={500} height={100} />
-                        <div className={style.slogan}>
-                            Мы воплощаем самые смелые мечты в реальность
+                <div className={style.headerTop}>
+                    <div className={style.headerLogo}>
+                        <Logo className={style.logo} />
+                        <div className={style.headerText}>
+                            <Name className={style.headerSpectr} />
+                            <div className={style.slogan}>
+                                Мы воплощаем самые смелые мечты в реальность
+                            </div>
                         </div>
+                    </div>
+                    <div className={style.slogan2}>
+                        Мы воплощаем самые смелые мечты в реальность
                     </div>
                 </div>
                 <div className={classnames(style.info, style.columnContainer)}>
@@ -29,13 +34,14 @@ export const MainPage = () => {
                 </div>
             </div>
             <div className={style.columnContainer}>
-                <p>{`Агродрон как важный инструмент для поддержания 
+                <p>
+                    {`Агродрон как важный инструмент для поддержания 
                     сельскохозяйственного производства путем внесения средств защиты растений (СЗР),
                     десикации и питания растений в ключевые периоды роста. Это устройство 
                     позволяет эффективно соблюдать график обработок, 
                     что особенно актуально в условиях меняющихся погодных условий и 
                     необходимости точного контроля над растениями.`}
-                 </p>
+                </p>
             </div>
         </>
     );
